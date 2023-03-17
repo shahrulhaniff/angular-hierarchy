cf40d2b2b3b485880a384906f3dfd8838b7b0af4
Shahrul Haniff <shahrul941013@gmail.com>
2023-03-18 05:07:40 +0800

    NgxOrgChartModule
     "enableIvy": false,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 // import { HomeComponent } from './pages/home/home.component';
+import { NgxOrgChartModule } from 'ngx-org-chart';
+// import { NgxOrgChartModule } from 'ngx-org-chart/lib/ngx-org-chart/ngx-org-chart.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxOrgChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
