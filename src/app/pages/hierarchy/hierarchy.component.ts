cee1ceb0ebb4e11fc7cfd1113e8bd5f07cfd22d1
Shahrul Haniff <shahrul941013@gmail.com>
2023-03-18 04:19:24 +0800

    test v1

--- src/app/pages/hierarchy/hierarchy.component.ts
+++ src/app/pages/hierarchy/hierarchy.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Employee {
+  upline: string;
+  name: string;
+  position: string;
+  level: number;
+}
+
 @Component({
   selector: 'app-hierarchy',
   templateUrl: './hierarchy.component.html',
@@ -11,5 +18,85 @@ export class HierarchyComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  myDownline = [
+    {
+      upline : "Me",
+      name : "John",
+      position : "Project Manager",
+      level   : 1
+    },
+    {
+      upline : "John",
+      name : "Reese",
+      position : "Assistant Manager",
+      level   : 2
+    },
+    {
+      upline : "Reese",
+      name : "Kyle",
+      position : "Developer",
+      level   : 3
+    },
+    {
+      upline : "Me",
+      name : "Sarah",
+      position : "Secretary",
+      level   : 1
+    },
+    {
+      upline : "Sarah",
+      name : "Layla",
+      position : "Writer",
+      level   : 2
+    },
+    {
+      upline : "Sarah",
+      name : "Eddie",
+      position : "Reporter",
+      level   : 2
+    },
+  ];
+
+
+  // emp
+  employees: Employee[] = [
+    {
+      upline : "Me",
+      name : "John",
+      position : "Project Manager",
+      level   : 1
+    },
+    {
+      upline : "John",
+      name : "Reese",
+      position : "Assistant Manager",
+      level   : 2
+    },
+    {
+      upline : "Reese",
+      name : "Kyle",
+      position : "Developer",
+      level   : 3
+    },
+    {
+      upline : "Me",
+      name : "Sarah",
+      position : "Secretary",
+      level   : 1
+    },
+    {
+      upline : "Sarah",
+      name : "Layla",
+      position : "Writer",
+      level   : 2
+    },
+    {
+      upline : "Sarah",
+      name : "Eddie",
+      position : "Reporter",
+      level   : 2
+    },
+  ];
 
 }
